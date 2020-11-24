fc0f846ebb559da7c66b3f1f45e16285378aadc8
boualimed <bmbouali@gmail.com>
2020-11-24 15:32:41 +0100

    bio has not worked

--- src/components/bio.js
+++ src/components/bio.js
@@ -55,7 +55,7 @@ const Bio = () => {
         <p>
           Written by <strong>{author.name}</strong> {author?.summary || null}
           {` `}
-          <a href={`https://github.com/${social?.twitter || ``}`}>
+          <a href={`https://github.com/boualimed${social?.twitter || ``}`}>
           You should follow me on Github
           </a>
         </p>
